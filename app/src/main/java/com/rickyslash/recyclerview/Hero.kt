fa070044aafeab5ff9a286690251a59fedb4e811
ricky-kiva <ricky.kiva@gmail.com>
2023-03-05 21:31:52 +0700

    add Hero dataClass

---
+++ app/src/main/java/com/rickyslash/recyclerview/Hero.kt
@@ -0,0 +1,11 @@
+package com.rickyslash.recyclerview
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Hero(
+    val name: String,
+    val desc: String,
+    val photo: Int
+) : Parcelable
