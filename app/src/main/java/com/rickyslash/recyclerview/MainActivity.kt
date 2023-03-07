63126c02fd723fc6c649e35193d67623f5c4e2cf
ricky-kiva <ricky.kiva@gmail.com>
2023-03-07 12:44:05 +0700

    add Menu to change RV Layout

--- app/src/main/java/com/rickyslash/recyclerview/MainActivity.kt
+++ app/src/main/java/com/rickyslash/recyclerview/MainActivity.kt
@@ -2,7 +2,10 @@ package com.rickyslash.recyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Toast
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -34,6 +37,27 @@ class MainActivity : AppCompatActivity() {
 
         // custom method (made by us) to 'show' the 'RecyclerView'
         showRecyclerList()
+    }
+
+    // add 'option menu' onCreate
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        // menuInflater Instantiates 'menu XML file' to its corresponding View objects
+        // 1st parameter is the 'XML menu' file, the 2nd is the 'menu' to inflate to
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    // add 'action' when 'option item' is 'selected'
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_list -> {
+                rvHeroes.layoutManager = LinearLayoutManager(this)
+            }
+            R.id.action_grid -> {
+                rvHeroes.layoutManager = GridLayoutManager(this, 2)
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     private fun getListHeroes(): ArrayList<Hero> {
